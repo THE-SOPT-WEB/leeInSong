ddd611571af1f43206dd49dcfe4fdfd6c129bb92
ingong <insong1216@gmail.com>
2022-04-22 18:27:16 +0900

    [feat]: quiz-app 구현

--- main.js
+++ main.js
@@ -6,8 +6,9 @@ import pic5 from "./assets/백지연.png";
 
 const $ = (selector) => document.querySelector(selector);
 
+// 처음 진입 시
 let currentStep = 0;
-
+let currentScore = 0;
 const quizList = [
   {
     src: pic1,
@@ -30,3 +31,92 @@ const quizList = [
     answer: "백지연",
   },
 ];
+window.addEventListener('DOMContentLoaded', () => {
+  addNewImage();
+});
+
+// 보기 선택
+const $list = $('.answer__list');
+const $imageBoard = $('.imageBoard');
+$list.addEventListener('click', ({target}) => {
+  target.closest('li') && handleOptionClick(target.closest('li')); 
+})
+
+function handleOptionClick(target){
+  const answerName = quizList[currentStep].answer;
+  const targetName = target.textContent;
+  const isCorrect = answerName === targetName;
+  
+  if (!isCorrect) showNotCorrectModal();
+  else {
+    plusScore();
+    handleNextStage();
+  }
+}
+
+function showNotCorrectModal() {
+  $('.modal__body').textContent = '나를 모른다고?';
+  $('.modal').classList.remove('hide');
+  removeNotCorrectModal();
+}
+
+function removeNotCorrectModal() {
+  setTimeout(() => {
+    $('.modal').classList.add('hide');
+  }, 1000)
+}
+
+function plusScore() {
+  currentScore++;
+  $('.scoreBoard__score').textContent = currentScore;
+}
+
+// 다음 단계 이동
+function handleNextStage() {
+  if (currentStep === 4) handleLastStage();
+  else {
+    currentStep++;
+    removeCurrentImage();
+    addNewImage();
+  }  
+}
+
+function removeCurrentImage(){
+  $imageBoard.replaceChildren();
+}
+
+function addNewImage() {
+  const imageElement = document.createElement('img');
+  imageElement.src = quizList[currentStep].src;
+  imageElement.alt = 'who is this';
+  $imageBoard.appendChild(imageElement);
+}
+
+function handleLastStage() {
+  $('.modal__body').textContent = '메인 화면으로';
+  $('.modal').classList.remove('hide');
+  addEventReturnClick();
+}
+
+function addEventReturnClick() {
+  $('.modal').addEventListener('click', resetStage);
+}
+
+// 다시 하기 
+$('.buttonList__shuffle').addEventListener('click', resetStage);
+function resetStage() {
+  resetStep();
+  resetScore();
+  removeCurrentImage();
+  addNewImage();
+  $('.modal').classList.add('hide');
+}
+
+function resetStep() {
+  currentStep = 0;
+}
+
+function resetScore() {
+  currentScore = 0;
+  $('.scoreBoard__score').textContent = currentScore;
+}
